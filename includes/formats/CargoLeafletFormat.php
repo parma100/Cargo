c791d08324935250d5b8251b429d1e0c2c044c3c
parma100 <87457725+parma100@users.noreply.github.com>
2021-07-14 23:30:34 -0400

    Update CargoLeafletFormat.php

--- includes/formats/CargoLeafletFormat.php
+++ includes/formats/CargoLeafletFormat.php
@@ -6,23 +6,30 @@
  */
 class CargoLeafletFormat extends CargoMapsFormat {
 
-	public function __construct( $output ) {
-		parent::__construct( $output );
-		self::$mappingService = "Leaflet";
-	}
+        public function __construct( $output ) {
+                parent::__construct( $output );
+                self::$mappingService = "Leaflet";
+        }
 
-	public static function allowedParameters() {
-		$allowedParams = parent::allowedParameters();
-		$allowedParams['image'] = [ 'type' => 'string' ];
-		return $allowedParams;
-	}
+        public static function allowedParameters() {
+                $allowedParams = parent::allowedParameters();
+                $allowedParams['image'] = [ 'type' => 'string' ];
+                return $allowedParams;
+        }
 
-	public static function getScripts() {
-		return [ "https://unpkg.com/leaflet@1.1.0/dist/leaflet.js" ];
-	}
+        public static function getScripts() {
+                $scripts = array();
+                array_push($scripts, "https://unpkg.com/leaflet@1.7.1/dist/leaflet.js");
+                array_push($scripts, "https://unpkg.com/leaflet.markercluster@1.4.1/dist/leaflet.markercluster.js");
+                return $scripts;
+        }
 
-	public static function getStyles() {
-		return [ "https://unpkg.com/leaflet@1.1.0/dist/leaflet.css" ];
-	}
+        public static function getStyles() {
+                $styles = array();
+                array_push($styles,"https://unpkg.com/leaflet@1.7.1/dist/leaflet.css");
+                array_push($styles,"https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.css");
+                array_push($styles,"https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.Default.css");
+                return $styles;
+        }
 
 }
